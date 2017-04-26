f50e48a696b596a2b7f71d7c570a87522c8db2a2
Sean Felling <Snfelling7@gmail.com>
2017-04-26 08:36:24 -0500

    final commit

--- solution.sql
+++ solution.sql
@@ -40,9 +40,9 @@ UPDATE syntax_practice set account_balance='10.00';
 -- 4. Select all users that have attempted 9 or more transactions.
 SELECT * FROM syntax_practice WHERE transactions_attempted >= 9;
 -- 5. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
-
+SELECT * FROM syntax_practice ORDER BY account_balance DESC LIMIT 3;
 -- 6. Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
-
+SELECT * FROM syntax_practice ORDER BY account_balance  LIMIT 3;
 -- 7. Get all users with account balances that are more than $100.
 SELECT * FROM syntax_practice WHERE account_balance >100.00;
 -- 8. Add a new record.
